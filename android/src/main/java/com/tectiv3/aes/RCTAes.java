3e1c65d129952cbedf987c7293286743b0559108
gessic-apps <abdul.osman@gessic.com>
2024-05-26 10:58:02 +0300

    android name refactor

--- android/src/main/java/com/tectiv3/aes/RCTAes.java
+++ android/src/main/java/com/tectiv3/aes/RCTAes.java
@@ -55,7 +55,7 @@ public class RCTAes extends ReactContextBaseJavaModule {
 
     @Override
     public String getName() {
-        return "RCTAes";
+        return "RCTUIAes";
     }
 
     @ReactMethod
